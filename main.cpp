a20f50c70ca3f63c523d0b9735ae7b41f10eb6f6
Rob Hess <robwhess@gmail.com>
2018-02-28 13:10:52 -0800

    Add code from 2/28 lecture.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,26 @@ void swap(T& a, T& b) {
   b = tmp;
 }
 
+template <class T>
+void selection_sort(std::vector<T>& array) {
+
+  for (int i = 0; i < array.size() - 1; i++) {
+
+    int min_idx = i;
+    for (int j = i + 1; j < array.size(); j++) {
+      if (array[j] < array[min_idx]) {
+        min_idx = j;
+      }
+    }
+
+    if (min_idx != i) {
+      swap(array[min_idx], array[i]);
+    }
+
+  }
+
+}
+
 
 int main() {
   std::vector<int> vec;
@@ -17,6 +37,14 @@ int main() {
   }
 
   std::cout << "Before sorting:";
+  for (int i = 0; i < vec.size(); i++) {
+    std::cout << "  " << vec[i];
+  }
+  std::cout << std::endl;
+
+  selection_sort(vec);
+
+  std::cout << "After sorting:";
   for (int i = 0; i < vec.size(); i++) {
     std::cout << "  " << vec[i];
   }
